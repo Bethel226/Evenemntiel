ac32355b89cb1ee1b78016cbc0c747100339687f
Bethel Ngakoutou <ngaradoumbethel@gmail.com>
2022-12-28 09:40:54 +0000

    Nouveau2

---
+++ src/app/modules/general/voirplus/voirplus.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VoirplusComponent } from './voirplus.component';
+
+describe('VoirplusComponent', () => {
+  let component: VoirplusComponent;
+  let fixture: ComponentFixture<VoirplusComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VoirplusComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(VoirplusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
